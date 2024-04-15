50ea9b8b83ed576951ef70e0d3c10bf394cf9358
slavalinwood <slavalinwood@gmail.com>
2024-04-16 02:49:07 +0300

    Добавляет функцию для поиска конкретных узлов списка
    Ищет по родителю, классу и тексту

--- js/form.js
+++ js/form.js
@@ -6,5 +6,13 @@ const priceInput = form.querySelector('#price');
 const checkInSelect = form.querySelector('#timein');
 const checkoutSelect = form.querySelector('#timeout');
 
-const advertsList = Object.entries(mapCanvas.children);
-
+const createNeededOffersArray = (data, offerClassName, offerText) => {
+  const allOffers = data.querySelectorAll(`.${offerClassName}`);
+  let neededOffers = [];
+  allOffers.forEach((elem) => {
+    if(elem.textContent === offerText) {
+      neededOffers.push(elem)
+    }
+  })
+  return neededOffers
+};
