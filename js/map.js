19177bcad8773606d05d6779a0d4cf7b77746d87
slavalinwood <slavalinwood@gmail.com>
2024-04-30 17:43:53 +0300

    Создаёт и добавляет на карту главный пин

--- js/map.js
+++ js/map.js
@@ -4,25 +4,44 @@ import {form, formFieldsets} from './form.js';
 const mapFilters = document.querySelector('.map__filters');
 const mapFiltersSelects = mapFilters.children;
 
+mapFilters.classList.add('map__filters--disabled');
+disableForm(mapFiltersSelects);
+
 const onMapLoad = () => {
   enableForm(mapFiltersSelects);
   enableForm(formFieldsets);
   mapFilters.classList.remove('map__filters--disabled');
   form.classList.remove('ad-form--disabled');
-};
-
-mapFilters.classList.add('map__filters--disabled');
-disableForm(mapFiltersSelects);
+}; 
 
 const map = L.map('map-canvas').on('load', onMapLoad).setView({
-  lat:  35.652832,
+  lat: 35.652832,
   lng: 139.839478,
 }, 10);
 
-L.tileLayer(
+const mapLayer = L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
-).addTo(map);
+);
 
+const mainPinIcon = L.icon({
+  iconUrl: 'keksobooking-leaflet/leaflet/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.652832,
+    lng: 139.839478,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mapLayer.addTo(map);
+marker.addTo(map);
