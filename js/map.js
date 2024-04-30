cc191315b37470df3ced3ab41470dbeb2eca5aa6
slavalinwood <slavalinwood@gmail.com>
2024-04-30 15:54:53 +0300

    Добавляет карту центра Токио на страницу

--- js/map.js
+++ js/map.js
@@ -1,7 +1,19 @@
-import { disableForm } from "./form.js";
+import { disableForm } from './form.js';
 
 const mapFilters = document.querySelector('.map__filters');
 const mapFiltersSelects = mapFilters.children;
 
 mapFilters.classList.add('map__filters--disabled');
 disableForm(mapFiltersSelects);
+
+const map = L.map('map-canvas').setView({
+  lat:  35.652832,
+  lng: 139.839478,
+}, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
