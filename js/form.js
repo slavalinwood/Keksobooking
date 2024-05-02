064d94d729ffb9abec5bb667a2bcd8709f80e100
slavalinwood <slavalinwood@gmail.com>
2024-05-02 16:19:09 +0300

    Выставляет заголовку атрибуты минимальной и максимальной длин, подписывает селект выбора кол-ва комнат на событие change.
    При выборе числа комнат ограничивается число гостей, где максимальное число - число выбранных комнат

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 import {disableForm} from './util.js';
 
-const MAX_PRICE = 1000000;
 const HOUSING_STARTING_PRICE = {
   'bungalow': 0,
   'flat': 1000,
@@ -16,6 +15,8 @@ const priceInput = form.querySelector('#price');
 const timeFieldset = form.querySelector('.ad-form__element--time');
 const checkInSelect = timeFieldset.querySelector('#timein');
 const checkoutSelect = timeFieldset.querySelector('#timeout');
+const roomsSelect = form.querySelector('#room_number');
+const guestsSelect = form.querySelector('#capacity');
 
 const onHousingSelectChange = (evt) => {
   priceInput.value = '';
@@ -28,13 +29,30 @@ const onTimeFieldsetChange = (evt) => {
   checkInSelect.value = evt.target.value;
 };
 
+const onRoomsSelectChange = (evt) => {
+  guestsSelect.value = evt.target.value;
+  const guestsOptions = guestsSelect.children;
+  for (let i = 0; i < guestsOptions.length; i++) {
+    const currentOption = guestsOptions[i];
+    if (currentOption.value > evt.target.value) {
+      currentOption.disabled = true;
+    }else {
+      currentOption.disabled = false;
+    }
+  }
+};
+
 form.classList.add('ad-form--disabled');
 disableForm(formFieldsets);
+
 priceInput.min = HOUSING_STARTING_PRICE[selectedHousing.value];
-priceInput.max = MAX_PRICE;
 priceInput.placeholder = HOUSING_STARTING_PRICE[selectedHousing.value];
 
 housingSelect.addEventListener('change', onHousingSelectChange);
 timeFieldset.addEventListener('change', onTimeFieldsetChange);
+roomsSelect.addEventListener('change', onRoomsSelectChange);
+guestsSelect.addEventListener('change', () => {});
+
+
 
 export {form, formFieldsets};
